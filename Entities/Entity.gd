62a7417484dd066161d9955f8760b54324d64033
Thomas <tajj.janssen@proton.me>
2026-07-17 10:44:17 +0200

    Added basic entity

---
+++ Entities/Entity.gd
@@ -0,0 +1,23 @@
+# Let op! Alleen implementaties van Entity gebruiken, dit is een basis class om te extenden
+
+@abstract 
+class_name GameEntity
+extends Node
+
+var health: float:
+	get: return health
+	set(value): health = value
+var damage: float:
+	get: return damage
+	set(value): damage = value
+var entity_name: String:
+	get: return entity_name
+	set(value): entity_name = value
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
